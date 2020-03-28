9732f362a11b771a42c961e4e988d9e9dc289aae
Wasim Saeed <wasim@elaghil.com>
2020-03-28 10:49:08 +0300

    design partners forms and the first time to Load project to github

---
+++ HumanResource/Employees/frmPartnerDetails.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HumanResource.Employees
+{
+    /// <summary>
+    /// Interaction logic for frmLogin.xaml
+    /// </summary>
+    public partial class frmPartnerDetails : Window
+    {
+        public frmPartnerDetails()
+        {
+            InitializeComponent();
+        }
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void Image_MouseDown_1(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left && e.ClickCount == 2)
+                this.Close();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnUndo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void lstPARTNER_NAME_LostFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void lstPARTNER_NAME_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
